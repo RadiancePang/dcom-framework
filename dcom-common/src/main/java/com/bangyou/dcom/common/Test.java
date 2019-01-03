459b3b6c2d7145821710850c40212f2eee26005f
RadinacePang <radiancepang@gmail.com>
2019-01-03 15:52:24 +0800

    first commit

---
+++ dcom-common/src/main/java/com/bangyou/dcom/common/Test.java
@@ -0,0 +1,19 @@
+package com.bangyou.dcom.common;
+
+import java.util.concurrent.locks.Lock;
+
+/**
+ * 测试
+ *
+ * @author radiance
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        Lock lock = Lock
+
+
+    }
+
+}
